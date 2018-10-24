dbcf1c2d4648eb944f1b741c88f773689e3bb756
Alexander Tepe <alexander.tepe@hotmail.de>
2018-10-24 13:01:58 +0200

    updated note

--- Blatt01/Rucksack/knapsack/Solution.java
+++ Blatt01/Rucksack/knapsack/Solution.java
@@ -28,7 +28,7 @@ public class Solution extends GenericSolution<Integer> {
 		assert sol.size() > item : "Item number " + item + " not found!";
 		assert sol.get(item) != null : "Item " + item + " not initialized in solution.";
 		// TODO: update solValue, solWeight, sol[item]
-		// Note: functions to alter solution variables located in instance.java
+		// Note: values are visible in this class.
 	}
 
 	/**
@@ -38,6 +38,11 @@ public class Solution extends GenericSolution<Integer> {
 	public boolean isFeasible() {
 		// TODO: implement this method (and you may then use it in your Solvers)
 		// NOTE: check if the current weight is higher than supported weight
+		int weight = 0;
+		for (int i = 0; i < instance.getWeightArray().length(), i++) {
+			weight += instance.getWeight[i];
+		}
+		return weight <= instance.getCapacity();
 	}
 
 	/**
