1e9b4578dbb481d4ea04adbba73eca1796476c88
Alexander Tepe <alexander.tepe@hotmail.de>
2018-11-05 11:41:48 +0100

    added template for binary solver

---
+++ Blatt02/Rucksack2/knapsack/BinarySolver.java
@@ -0,0 +1,15 @@
+package knapsack;
+
+public class BinarySolution implements Solverintterface {
+
+    private Instance instance;
+
+    public BinarySolution(Instance instance) {
+        this.instance = instance;
+    }
+
+    public Solution solve(Instance instance) {
+        // TODO implement this
+        return null;
+    }
+}
